02b5e878d698348496e3cf1e4f26318a0b158dcb
Richard Fagan <rfagan1@hawk.iit.edu>
2018-11-12 14:10:51 -0600

    Fix lint error by addin 'use strict' global

--- pp-04/site.js
+++ pp-04/site.js
@@ -1,9 +1,10 @@
+'use strict';
 // Return if no JavaScript
 (function(){
   if (typeof document.querySelector === 'undefined') {
     return;
   }
-})
+});
 function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
