fba5ea5109565e874baa69bfcac75d564868adf1
Richard Fagan <rfagan1@hawk.iit.edu>
2018-11-12 12:37:36 -0600

    Add check for JavaScript

--- pp-04/site.js
+++ pp-04/site.js
@@ -1,3 +1,9 @@
+// Return if no JavaScript
+(function(){
+  if (typeof document.querySelector === 'undefined') {
+    return;
+  }
+})
 function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
