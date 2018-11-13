e49ff8986a8e8ef75fe44e8581d9f1f68c7e970f
Richard Fagan <rfagan1@hawk.iit.edu>
2018-11-12 20:53:39 -0600

    Add logic to ring bell via JavaScript

--- pp-04/site.js
+++ pp-04/site.js
@@ -6,6 +6,15 @@
   }
 });
 function ringDoorbell() {
+  console.log('Click Event');
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
-}
+};
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('DOM loaded');
+  //  Check for id in html
+  var bell = document.getElementById("ring-bell");
+
+  // Call function to ring bell if id found
+  bell.addEventListener('click', ringDoorbell);
+});
