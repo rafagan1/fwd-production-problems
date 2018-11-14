e719784564971e0f54f42c27bf4401462d0cf8d6
Richard Fagan <rfagan1@hawk.iit.edu>
2018-11-14 16:10:45 -0600

    Update site.js

--- pp-04/site.js
+++ pp-04/site.js
@@ -1,4 +1,24 @@
-function ringDoorbell() {
-  var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
-}
+'use strict'
+// Return if no JavaScript
+(function() {
+  var doorbell;
+  var bell;
+  if (typeof document.querySelector === 'undefined') {
+    return;
+  }
+
+  function ringDoorbell() {
+    console.log('Click Event');
+    doorbell = new Audio('media/doorbell.mp3');
+    doorbell.play();
+  }
+
+  document.addEventListener('DOMContentLoaded', function(){
+    console.log('DOM loaded');
+    // Add button element to html
+    document.querySelector('#ring').innerHTML =
+    "<button id='door-button'>Ring Doorbell</button>";
+    bell = document.getElementById("door-button");
+    bell.addEventListener('click', ringDoorbell);
+  });
+})();
