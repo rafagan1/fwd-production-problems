ef5ece64d586148ef8dd52b277d8e39bba8d7ac8
Richard Fagan <rfagan1@hawk.iit.edu>
2018-11-15 16:00:46 -0600

    Add finger pointer

--- pp-04/site.js
+++ pp-04/site.js
@@ -1,23 +1,37 @@
 'use strict';
 // Return if no JavaScript
 (function() {
-  var doorbell;
-  var bell;
+  var doorbell, bell;
+  var mybell;
+  var mynav;
+  var html;
+  var dbutton;
+
   if (typeof document.querySelector === 'undefined') {
     return;
   }
 
+  html = document.querySelector('html');
+  html.classList.remove('nojs');
+  html.classList.add('js');
+
+  mybell = document.querySelector('#ring');
+  mynav = document.querySelector('.mynav');
+
   function ringDoorbell() {
     console.log('Click Event');
+    mynav.classList.toggle('visible');
     doorbell = new Audio('media/doorbell.mp3');
     doorbell.play();
   }
 
   document.addEventListener('DOMContentLoaded', function(){
     console.log('DOM loaded');
-    // Add button element to html
-    document.querySelector('#ring').innerHTML =
-    "<button id='door-button'>Ring Doorbell</button>";
+    dbutton = document.createElement('button');
+    dbutton.setAttribute('id', 'door-button');
+    dbutton.textContent = 'Press to Ring';
+
+    mybell.insertBefore(dbutton, mynav);
     bell = document.getElementById("door-button");
     bell.addEventListener('click', ringDoorbell);
   });
